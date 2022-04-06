784d7cf66d3f7016ed333dca4437df2f897c8adb
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-04-06 22:38:14 +0900

    [ADD] 0406 문자열을 정수로 바꾸기

---
+++ src/programmers/integer.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class integer {
+
+    public int solution(String s) {
+        int answer = 0;
+        return Integer.valueOf(s);
+        //integer 클래스안에 valueOf메소드가 만들어져있음.
+        //s를 int로 바꿔서 return 해줌.
+    }
+}
