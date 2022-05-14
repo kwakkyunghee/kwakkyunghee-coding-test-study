559821ea355cc969ba5bdb5042064600e97aac3a
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-05-15 02:52:43 +0900

    [0512] 콜라츠 추측 (#53)
    * [0512] 콜라츠 추측
    
    * [0512] 콜라츠 추측
    
    * [0512] 콜라츠 추측

---
+++ src/programmers/Solution0512.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class Solution0512 {
+    public int solution(int n) {
+        long num=n;
+        int answer = 0;
+
+        while(num!=1) {
+            if(num%2==0) {
+                num /= 2;
+            } else {
+                num = 3*num + 1;
+            }
+
+            answer++;
+            if(answer>500){
+                return-1;
+            }
+        }
+        return answer;//
+    }
+}
