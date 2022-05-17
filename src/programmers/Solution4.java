b32962e8aa9fb1a7dc2b8f5b4e7979d7f8813bf4
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-05-17 09:20:54 +0900

    [0516] 음양 더하기 (#57)

---
+++ src/programmers/Solution4.java
@@ -0,0 +1,16 @@
+package programmers;
+
+class Solution4 {
+    static int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < signs.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
